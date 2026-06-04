05ffeee5dec72a838f583b6d519778d9202dc594
Michal Šotek <msotek@blumenbecker.com>
2026-06-04 09:13:03 +0200

    Css styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.profijeraby.cz',
+  site: 'https://sotemi.github.io',
+  base: '/ProfiJeraby/',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
